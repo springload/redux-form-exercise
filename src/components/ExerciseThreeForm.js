c66e176020c70a728ffc7ca441bd50aeb77adebb
Katie Day <katie@springload.co.nz>
2017-03-07 15:50:57 +1300

    adding copy formatting - easier to read

--- src/components/ExerciseThreeForm.js
+++ src/components/ExerciseThreeForm.js
@@ -7,23 +7,22 @@ const ExerciseThreeForm = () => (
     <div>
         <div>
             <h3>Exercise 3</h3>
-            <p>
-                I need to be a form that takes an input (using InputField component) name
-                and a textarea (using a TextareaField component that you have to create).<br />
-                Label for input is "Your name:" and label for textarea is
-                "Your text:"<br />
-                Please make sure the name is required and raise a warning if it is not only alphabetic string.<br />
-                The textarea field is required, must be alphanumeric and allow spaces, "'" and "." characters. Raise an error if one of these conditions isn't respected.<br />
-                You will raise a warning if the text doesn't contain the sentence 'This is super cool.'<br />
-                Result must show something like that:<br />
-            </p>
+            <p>Create a form that takes an input (using InputField component) with name="name", and a textarea (create your own TextareaField component) with name="text". </p>
+            <p>Label your input as "Your name:" and your textarea as "Your text:"</p>
+            <p>Add some validation:</p>
+            <ul>
+                <li>Both the input and text area are required</li>
+                <li>Raise a warning if the input is not only alphabetic string.</li>
+                <li>The textarea <i>must</i> be alphanumeric and allow spaces, fullstops and quotation marks. Raise an error if these conditions are not meet.</li>
+                <li>Raise a warning in the textarea if the text does not contain the sentence 'This is super cool.'</li>
+            </ul>
+            <p>A successful form submission should show something like this:</p>
             <div style={{ border: '1px solid black', paddingLeft: '10px' }}>
                 <p>TheGuyWhoMadeThisExercise has written this:</p>
                 <p>Probably the best example I've ever seen on the internet. This is super cool. 34 is a number.</p>
             </div>
             <p>
-                TODO: Implement me in actions, reducers, containers, /components/Form/TextareaField.js &amp;<br />
-                Modify /components/ExerciseThreeForm.js
+                TODO: Implement me in actions, reducers, containers, /components/Form/TextareaField.js &amp; modify /components/ExerciseThreeForm.js
             </p>
         </div>
         {
